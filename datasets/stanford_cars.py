caad63c10ea279da2b8819eeb406ac2f5924e01c
Aditya Kusupati <adityakusupati2@gmail.com>
2023-05-16 10:42:29 -0700

    OpenCLIP

--- datasets/stanford_cars.py
+++ datasets/stanford_cars.py
@@ -17,6 +17,7 @@ class StanfordCars(DatasetBase):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
         self.dataset_dir = os.path.join(root, self.dataset_dir)
         self.split_path = os.path.join(self.dataset_dir, "split_zhou_StanfordCars.json")
+        print(self.split_path)
         self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
         mkdir_if_missing(self.split_fewshot_dir)
 
